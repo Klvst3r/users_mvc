18ea9ee7c2729781e17cab00d43b7bec1c80ea3b
Klvst3r <klvst3r@gmail.com>
2017-02-24 12:57:42 -0600

    Create config DB file

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+* En este archivo de configuración crearemos un archivo en el que colocaremos la configuración de la base de datos. 
+Esta devuelve un array que posteriormente utilizaremos
+*/
+return array(
+	"driver"	=>"mysql",
+	"host"		=>"localhost",
+	"user"		=>"dev",
+	"pass"		=>"desarrollo",
+	"database"	=>"users_mvc",
+	"charset"	=>"utf8"
+);
+?>
