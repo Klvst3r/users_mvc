1cec97c93a6127b6361f6c433fb0acc3b4b703d4
Klvst3r <klvst3r@gmail.com>
2017-02-24 13:00:49 -0600

    Create Constants global file

---
+++ config/global.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* Archivo que sirve para las constantes que serviran para establecer controladores y acciones por defecto y todo lo que queramos meterle
+*/
+define("CONTROLADOR_DEFECTO", "Usuarios");
+define("ACCION_DEFECTO", "index");
+// Mas constantes de configuracion
+
+?>
